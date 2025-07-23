2bf3444eb79c4f177f747699465772922b6bc454
HyunBae Choi <hyunbaechoi93@gmail.com>
2025-07-23 15:07:44 -0700

    maintenance: updated TradeOpenSearchIndexKey to remove sequence

--- domain/domain.go
+++ domain/domain.go
@@ -4,6 +4,7 @@ import "fmt"
 
 // In Datastore, trades use the TXID value as a key component. However, this makes the key non-reproducible when a trade is updated, which prevents updating the indexed object in OpenSearch.
 // To address this, we use a different key for trade indexes in OpenSearch, allowing pending or partially filled "trades" to be updated.
-func TradeOpenSearchIndexKey(sequence int64, orderKey, orgID string) string { // Network is included in orderKey
-	return fmt.Sprintf("%d_%s_%s", sequence, orgID, orderKey)
+// OrderKey includes the network, as well as the order number, which is unique for each order.
+func TradeOpenSearchIndexKey(orderKey, orgID string) string {
+	return fmt.Sprintf("%s_%s", orgID, orderKey)
 }
