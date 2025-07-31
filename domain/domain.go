717a3a63c60ee6d09b273f02da2141297987bbf4
HyunBae Choi <hyunbaechoi93@gmail.com>
2025-07-31 11:07:35 -0700

    maintenance: updated TradeOpenSearchIndexKey

--- domain/domain.go
+++ domain/domain.go
@@ -1,10 +1,13 @@
 package domain
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/sologenic/com-fs-utils-lib/models/metadata"
+)
 
 // In Datastore, trades use the TXID value as a key component. However, this makes the key non-reproducible when a trade is updated, which prevents updating the indexed object in OpenSearch.
 // To address this, we use a different key for trade indexes in OpenSearch, allowing pending or partially filled "trades" to be updated.
-// OrderKey includes the network, as well as the order number, which is unique for each order.
-func TradeOpenSearchIndexKey(orderKey, orgID string) string {
-	return fmt.Sprintf("%s_%s", orgID, orderKey)
+func TradeOpenSearchIndexKey(userID, orgID string, sequence int64, network metadata.Network) string {
+	return fmt.Sprintf("%s_%d_%s_%d", userID, sequence, orgID, network)
 }
